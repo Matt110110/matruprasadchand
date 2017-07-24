4aecfb31fe35f7a091254dd0b2d1badcd4431665
Matt110110 <matruprasadchand@gmail.com>
2017-07-24 19:52:18 +0530

    The work experience shit is left

---
+++ script.js
@@ -0,0 +1,25 @@
+var linkedIn=  function() {
+    window.location="https://www.linkedin.com/in/matruprasad-chand-137a35133/";
+}
+
+var twitter = function () {
+    window.location="https://twitter.com/Matt110110";
+}
+
+var facebook = function () {
+    window.location="https://www.facebook.com/Matt110110";
+}
+
+var insta = function () {
+    window.location="https://www.instagram.com/matt110110/";
+}
+
+var github = function () {
+    window.location="https://github.com/Matt110110";
+}
+
+document.getElementById("linkedIn").addEventListener("click", linkedIn);
+document.getElementById("twitter").addEventListener("click", twitter);
+document.getElementById("facebook").addEventListener("click", facebook);
+document.getElementById("insta").addEventListener("click", insta);
+document.getElementById("github").addEventListener("click", github);
