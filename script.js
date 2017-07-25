f7ac60c76bf529a0d57f244f83bdb24e518a4d5a
Matt110110 <matruprasadchand@gmail.com>
2017-07-25 20:52:21 +0530

    Anonymous functions

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-var linkedIn=  function() {
-    window.location="https://www.linkedin.com/in/matruprasad-chand-137a35133/";
-}
-
-var twitter = function () {
+document.getElementById("linkedIn").addEventListener("click", function () {
+   window.location="https://www.linkedin.com/in/matruprasad-chand-137a35133/"; 
+});
+document.getElementById("twitter").addEventListener("click", function () {
     window.location="https://twitter.com/Matt110110";
-}
-
-var facebook = function () {
+});
+document.getElementById("facebook").addEventListener("click", function () {
     window.location="https://www.facebook.com/Matt110110";
-}
-
-var insta = function () {
+});
+document.getElementById("insta").addEventListener("click", function () {
     window.location="https://www.instagram.com/matt110110/";
-}
-
-var github = function () {
+});
+document.getElementById("github").addEventListener("click", function () {
     window.location="https://github.com/Matt110110";
-}
-
-document.getElementById("linkedIn").addEventListener("click", linkedIn);
-document.getElementById("twitter").addEventListener("click", twitter);
-document.getElementById("facebook").addEventListener("click", facebook);
-document.getElementById("insta").addEventListener("click", insta);
-document.getElementById("github").addEventListener("click", github);
+});
