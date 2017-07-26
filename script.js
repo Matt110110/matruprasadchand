0f704de61e2cffab1691bc1929667df713980e30
Matt110110 <matruprasadchand@gmail.com>
2017-07-26 14:36:29 +0530

    jQuery

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+$("document").ready(function () {
+    $("a[href^=mailto]").css("text-decoration", "none");
+    $("body").css("font-family", "Roboto");
+    $("h1").css("font-family", "Roboto");
+    $("h2").css("font-family", "Roboto");
+    $("h3").css("font-family", "Roboto");
+    $("h4").css("font-family", "Roboto");
+    $("h5").css("font-family", "Roboto");
+    $("h6").css("font-family", "Roboto");
+    $("#linkedIn").click(function () {
+        window.location="https://www.linkedin.com/in/matruprasad-chand-137a35133/";
+    });
+    $("#twitter").click(function () {
+        window.location="https://twitter.com/Matt110110";
+    });
+    $("#facebook").click(function () {
+        window.location="https://www.facebook.com/Matt110110";
+    });
+    $("#insta").click(function () {
+        window.location="https://www.instagram.com/matt110110/";
+    });
+    $("#github").click(function () {
+        window.location="https://github.com/Matt110110";
+    });
+});
+/*
 document.getElementById("linkedIn").addEventListener("click", function () {
    window.location="https://www.linkedin.com/in/matruprasad-chand-137a35133/"; 
 });
@@ -13,3 +39,4 @@ document.getElementById("insta").addEventListener("click", function () {
 document.getElementById("github").addEventListener("click", function () {
     window.location="https://github.com/Matt110110";
 });
+*/
