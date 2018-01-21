bba9cfef18ad8f8f727ac7ed76a499bcfe0e9528
Matt110110 <matruprasadchand@gmail.com>
2018-01-21 13:53:36 +0530

    This file has some changes that I am not sure about

--- script.js
+++ script.js
@@ -27,4 +27,62 @@ $("document").ready(function () {
     $("#insta").addClass("w3-text-pink");
     $("#linkedIn").addClass("w3-text-aqua");
     $("#WE").hide();
+});$("document").ready(function () {
+    $("a[href^=mailto]").css("text-decoration", "none");
+    $("body").css("font-family", "Roboto");
+    $("h1").css("font-family", "Roboto");
+    $("h2").css("font-family", "Roboto");
+    $("h3").css("font-family", "Roboto");
+    $("h4").css("font-family", "Roboto");
+    $("h5").css("font-family", "Roboto");
+    $("h6").css("font-family", "Roboto");
+    $("#linkedIn").click(function () {
+        window.location="https://www.linkedin.com/in/matruprasad-chand-137a35133/";
+    });
+    $("#twitter").click(function () {
+        window.location="https://twitter.com/Matt110110";
+    });
+    $("#facebook").click(function () {
+        window.location="https://www.facebook.com/Matt110110";
+    });
+    $("#insta").click(function () {
+        window.location="https://www.instagram.com/matt110110/";
+    });
+    $("#github").click(function () {
+        window.location="https://github.com/Matt110110";
+    });
+    $("#facebook").addClass("w3-text-blue");
+    $("#twitter").addClass("w3-text-indigo");
+    $("#insta").addClass("w3-text-pink");
+    $("#linkedIn").addClass("w3-text-aqua");
+    $("#WE").hide();
+});$("document").ready(function () {
+    $("a[href^=mailto]").css("text-decoration", "none");
+    $("body").css("font-family", "Roboto");
+    $("h1").css("font-family", "Roboto");
+    $("h2").css("font-family", "Roboto");
+    $("h3").css("font-family", "Roboto");
+    $("h4").css("font-family", "Roboto");
+    $("h5").css("font-family", "Roboto");
+    $("h6").css("font-family", "Roboto");
+    $("#linkedIn").click(function () {
+        window.location="https://www.linkedin.com/in/matruprasad-chand-137a35133/";
+    });
+    $("#twitter").click(function () {
+        window.location="https://twitter.com/Matt110110";
+    });
+    $("#facebook").click(function () {
+        window.location="https://www.facebook.com/Matt110110";
+    });
+    $("#insta").click(function () {
+        window.location="https://www.instagram.com/matt110110/";
+    });
+    $("#github").click(function () {
+        window.location="https://github.com/Matt110110";
+    });
+    $("#facebook").addClass("w3-text-blue");
+    $("#twitter").addClass("w3-text-indigo");
+    $("#insta").addClass("w3-text-pink");
+    $("#linkedIn").addClass("w3-text-aqua");
+    $("#WE").hide();
 });
